ad4aa8b31c14871c8bae7021cc92dbea708236b5
MichalLuzyna <michal.luzyna@capgemini.com>
2021-05-28 00:51:29 +0200

    async download

--- src/main/java/com/devonfw/java/training/nonblockingio/mvc/StorageControllerAsync.java
+++ src/main/java/com/devonfw/java/training/nonblockingio/mvc/StorageControllerAsync.java
@@ -2,17 +2,22 @@ package com.devonfw.java.training.nonblockingio.mvc;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.stream.Collectors;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -60,7 +65,42 @@ public class StorageControllerAsync {
   @ResponseBody
   public void downloadNonblocking(@PathVariable String filename, HttpServletRequest request,
       HttpServletResponse response) throws IOException {
+    String clientId = request.getParameter("clientId");
+    Resource file = this.storageService.loadAsResource(filename);
+    InputStream fileInput = file.getInputStream();
+    response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"");
 
+    AsyncContext context = request.startAsync(); // 1
+    context.setTimeout(0);
+    ServletOutputStream responeOutput = response.getOutputStream(); // 2
+
+    responeOutput.setWriteListener(new WriteListener() { // 3
+      @Override
+      public void onWritePossible() throws IOException {
+
+        byte[] buffer = new byte[1024];
+        while (responeOutput.isReady()) { // 4
+          int length = fileInput.read(buffer);
+          if (length > 0) {
+            LOG.info("Client {},  writing {} bytes", clientId, length);
+            responeOutput.write(buffer, 0, length); // 5
+          } else {
+            LOG.info("Client {} - flush", clientId);
+            responeOutput.flush(); // 6
+            context.complete(); // 7
+            return;
+          }
+        }
+
+      }
+
+      @Override
+      public void onError(Throwable t) { // 7
+
+        LOG.error("ERROR", t);
+        context.complete();
+      }
+    });
   }
 
   @PostMapping("upload-nio")
